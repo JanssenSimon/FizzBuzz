3e06f34da5bee93b64039d28e591be0271579a47
LordFarquadTheGreat <moniquetamere1@gmail.com>
2020-06-08 11:23:33 -0400

    Adds FizzBuzz functionality

--- FizzBuzz.Tests/UnitTest1.cs
+++ FizzBuzz.Tests/UnitTest1.cs
@@ -7,16 +7,37 @@ namespace FizzBuzz.Tests
     public class UnitTest1
     {
         [Fact]
-        public void ReturnInputAsString()
+        public void ReturnsInputAsString()
         {
             Assert.Equal("1", FizzBuzz.FromIndex(1));
+            Assert.Equal("16", FizzBuzz.FromIndex(16));
         }
 
         [Fact]
-        public void ReturnFizzOnThrees()
+        public void ReturnsFizzOnThrees()
         {
             Assert.Equal("Fizz", FizzBuzz.FromIndex(3));
             Assert.Equal("Fizz", FizzBuzz.FromIndex(6));
+        }
+
+        [Fact]
+        public void ReturnsBuzzOnFives()
+        {
+            Assert.Equal("Buzz", FizzBuzz.FromIndex(5));
+            Assert.Equal("Buzz", FizzBuzz.FromIndex(10));
+        }
+
+        [Fact]
+        public void ReturnsFizzBuzzOnFifteens()
+        {
+            Assert.Equal("FizzBuzz", FizzBuzz.FromIndex(15));
+            Assert.Equal("FizzBuzz", FizzBuzz.FromIndex(30));
+        }
+
+        [Fact]
+        public void PlaysUntilFifteen()
+        {
+            Assert.Equal("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz", FizzBuzz.PlayTo(15));
         }
     }
 }
--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,11 +6,30 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("FizzBuzz!");
+            Console.WriteLine(FizzBuzz.PlayTo(100));
         }
 
-        public static string FromIndex(int index) {
-            return index.ToString();
+        public static string PlayTo(int max)
+        {
+            string output = "";
+            for(int i = 1; i < max; i++)
+                output += FizzBuzz.FromIndex(i) + "\n";
+            output += FizzBuzz.FromIndex(max);
+            return output;
+        }
+
+        public static string FromIndex(int index) 
+        {
+            string output = "";
+            if(IsMultipleOf(3, index)) output += "Fizz";
+            if(IsMultipleOf(5, index)) output += "Buzz";
+            if(String.IsNullOrEmpty(output)) output += index.ToString();
+            return output;
+        }
+
+        private static bool IsMultipleOf(int factor, int number)
+        {
+            return number % factor == 0;
         }
     }
 }
